68e3240889bdc073b6aec08b22f85f0808845e2b
Guilherme Nunes <guinunesilva@gmail.com>
2014-12-01 22:21:10 -0200

    improved shell files

--- puphpet/files/exec-once/nginx-conf.sh
+++ puphpet/files/exec-once/nginx-conf.sh
@@ -2,5 +2,6 @@
 
 sudo rm -rf /etc/nginx/sites-available/*
 sudo rm -rf /etc/nginx/sites-enabled/*
-sudo cp /var/www/puphpet/files/exec-once/localhost.conf /etc/nginx/sites-enabled/localhost.conf
+sudo cp /var/www/puphpet/files/exec-once/localhost.conf /etc/nginx/sites-available/localhost.conf
+sudo ln -s /etc/nginx/sites-available/localhost.conf /etc/nginx/sites-enabled/localhost.conf
 sudo service nginx restart
--- puphpet/files/startup-always/pm2.sh
+++ puphpet/files/startup-always/pm2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #change the line below to automatic start your project
-#pm2 start /var/www/html/heroquest/app.js --watch
+#pm2 start /var/www/html/app_name/app.js --watch
